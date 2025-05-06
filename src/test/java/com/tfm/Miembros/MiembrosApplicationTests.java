0786174fbfcb78aff92734181cc5e842a1475158
MariaCiffu <mariaciffu@gmail.com>
2025-05-06 18:34:05 +0200

    fallo config 3

--- src/test/java/com/tfm/Miembros/MiembrosApplicationTests.java
+++ src/test/java/com/tfm/Miembros/MiembrosApplicationTests.java
@@ -2,12 +2,17 @@ package com.tfm.Miembros;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class MiembrosApplicationTests {
-
+@SpringBootTest(
+		properties = {
+				"eureka.client.enabled=false",
+				"spring.cloud.discovery.enabled=false"
+		}
+)
+public class MiembrosApplicationTests {
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		// Test vacío solo para verificar carga de contexto
 	}
-
 }
